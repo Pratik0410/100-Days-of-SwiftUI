9870c0b164d6556d2f45f8a0185ee19f2700c8a4
Prathamesh Sonawane <0410pratik@gmail.com>
2023-12-16 22:42:50 +0530

    Day 2 contents modified

--- Day 2/Day 2.playground/Contents.swift
+++ Day 2/Day 2.playground/Contents.swift
@@ -21,3 +21,26 @@ print(gameOver)
 
 gameOver.toggle()
 print(gameOver)
+
+let firstPart = "Hello, "
+let secondPart = "world!"
+let greeting = firstPart + secondPart
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+print(lyric)
+
+let luggageCode = "1" + "2" + "3" + "4" + "5"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let name = "Taylor"
+let age = 26
+let message = "Hello, my name is \(name) and I'm \(age) years old."
+print(message)
+
+let missionMessage = "Apollo " + String(number) + " landed on the moon."
+// let missionMessage = "Apollo \(number) landed on the moon."
+
+print("5 x 5 is \(5 * 5)")
