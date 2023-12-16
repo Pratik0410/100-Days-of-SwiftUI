ec10dd0e983e6ef881ad3e0a3ffdb5b0bd8e8d19
Prathamesh Sonawane <0410pratik@gmail.com>
2023-12-16 22:37:28 +0530

    Day 2 completed

---
+++ Day 2/Day 2.playground/Contents.swift
@@ -0,0 +1,23 @@
+import Cocoa
+
+let filename = "paris.jpg"
+print(filename.hasSuffix(".jpg"))
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+let goodDogs = true
+var gameOver = false
+
+let isMultiple = 120.isMultiple(of: 3)
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+
+print(gameOver)
+
+gameOver.toggle()
+print(gameOver)
