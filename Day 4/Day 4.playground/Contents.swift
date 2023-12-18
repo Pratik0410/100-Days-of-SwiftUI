97647dfec2095cbcb6e16f1662a47a630c26b926
Prathamesh Sonawane <0410pratik@gmail.com>
2023-12-18 20:00:23 +0530

    Complex Data Types, Part 2 completed

---
+++ Day 4/Day 4.playground/Contents.swift
@@ -0,0 +1,36 @@
+import Cocoa
+
+// Type Anotation
+
+let surname: String = "Lasso"
+var score1: Int = 0
+
+var score2 : Double = 0
+
+let playerName: String = "Roy"
+var luckyNumber: Int = 4
+let pi: Double = 3.141
+var isAuthenticated: Bool = true
+
+var albums: [String] = ["Red", "Fearless"]
+var user: [String: String] = ["id": "@twostraws"]
+var books: Set<String> = Set([
+    "The Bluest Eye",
+    "Foundation",
+    "Girl, Woman, Other"
+])
+var soda: [String] = ["Coke", "Pepsi", "Irn-Bru"]
+var teams: [String] = [String]()
+var cities: [String] = []
+var clues = [String]()
+
+enum UIStyle {
+    case light, dark, system
+}
+
+var style: UIStyle = UIStyle.light
+style = .dark
+
+let username: String
+username = "@PratikS0410"
+print(username)
