a963b0fdf4d99ad786c51a8dd751931d62c606f4
Prathamesh Sonawane <0410pratik@gmail.com>
2023-12-29 00:18:42 +0530

    Day 13 completed

--- Day 13/Day 13.playground/Contents.swift
+++ Day 13/Day 13.playground/Contents.swift
@@ -109,4 +109,32 @@ extension Book {
 
 let lotr = Book(title: "Lord of the Rings", pageCount: 1178, readingHours: 24)
 
+extension Collection {
+    var isNotEmpty: Bool {
+        isEmpty == false
+    }
+}
 
+let guests = ["Mario", "Luigi", "Peach"]
+
+if guests.isNotEmpty {
+    print("Guest count: \(guests.count)")
+}
+
+protocol Person {
+    var name: String { get }
+    func sayHello()
+}
+
+extension Person {
+    func sayHello() {
+        print("Hi, I'm \(name)")
+    }
+}
+
+struct Employee: Person {
+    let name: String
+}
+
+let taylor = Employee(name: "Taylor Swift")
+taylor.sayHello()
