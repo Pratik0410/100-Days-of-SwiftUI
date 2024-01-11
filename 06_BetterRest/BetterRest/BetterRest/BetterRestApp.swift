0eede375d4b380e03ce26a5b2d85bf49472d5049
Prathamesh Sonawane <0410pratik@gmail.com>
2024-01-11 23:14:42 +0530

    Initialize BetterRest project with trained ML model

---
+++ 06_BetterRest/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Prathamesh Sonawane on 11/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
