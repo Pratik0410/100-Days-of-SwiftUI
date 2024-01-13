37f1bf396a3dbbaec52d114bdf25e0d120d32824
Prathamesh Sonawane <0410pratik@gmail.com>
2024-01-14 00:00:28 +0530

    BetterRest final challenge completed

--- 06_BetterRest/BetterRest/BetterRest/ContentView.swift
+++ 06_BetterRest/BetterRest/BetterRest/ContentView.swift
@@ -6,16 +6,89 @@
 //
 
 import SwiftUI
+import CoreML
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? .now
+    }
+    
+    var bedTime: String {
+        
+        var bedTime = ""
+        
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            
+            let prediction = try model.prediction(wake: Int64(Double(hour + minute)), estimatedSleep: sleepAmount, coffee: Int64(Double(coffeeAmount)))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            let sleepTimeText = sleepTime.formatted(date: .omitted, time: .shortened)
+            bedTime = "Your ideal bedtime is \(sleepTimeText)"
+            
+        } catch {
+            bedTime = "Sorry, there was a problem calculating your bedtime."
         }
-        .padding()
+        
+        return bedTime
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                } header: {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                }
+                
+                Section {
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                } header: {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                }
+                
+                Section {
+                    Picker("Number of Cups", selection: $coffeeAmount) {
+                        ForEach(1..<21) {
+                            Text("^[\($0) cup](inflect: true)")
+                        }
+                    }
+                    .labelsHidden()
+                    .pickerStyle(.wheel)
+    
+                } header: {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                }
+                
+                Section {
+                    Text(bedTime).font(.headline)
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+                .multilineTextAlignment(.center)
+                
+            }
+            .navigationTitle("BetterRest")
+        }
     }
 }
 
