5a3c01dcf1e4764c3df6020572bac04a8f825103
Prathamesh Sonawane <0410pratik@gmail.com>
2024-01-14 21:26:19 +0530

    Initiate word scramble app project

---
+++ 07_Word_Scramble/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Prathamesh Sonawane on 14/01/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
