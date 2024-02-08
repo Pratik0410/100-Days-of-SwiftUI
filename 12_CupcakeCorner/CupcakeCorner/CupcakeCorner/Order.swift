ef53572c6c7711e2310d2594c57cfc48a2141147
Prathamesh Sonawane <0410pratik@gmail.com>
2024-02-09 01:02:07 +0530

    Implemented checkout process & URLSession for data exchange

--- 12_CupcakeCorner/CupcakeCorner/CupcakeCorner/Order.swift
+++ 12_CupcakeCorner/CupcakeCorner/CupcakeCorner/Order.swift
@@ -8,7 +8,19 @@
 import Foundation
 
 @Observable
-class Order {
+class Order: Codable {
+    enum CodingKeys: String, CodingKey {
+        case _type = "type"
+        case _quantity = "quantity"
+        case _specialRequestEnabled = "specialRequestEnabled"
+        case _extraFrosting = "extraFrosting"
+        case _addSprinkles = "addSprinkles"
+        case _name = "name"
+        case _city = "city"
+        case _streetAddress = "streetAddress"
+        case _zip = "zip"
+    }
+    
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
 
     var type = 0
@@ -25,4 +37,33 @@ class Order {
     
     var extraFrosting = false
     var addSprinkles = false
+    
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+
+        return true
+    }
+    
+    var cost: Double {
+        var cost = Double(quantity) * 2
+
+        cost += (Double(type) / 2)
+
+        if extraFrosting {
+            cost += Double(quantity)
+        }
+
+        if addSprinkles {
+            cost += Double(quantity) / 2
+        }
+
+        return cost
+    }
 }
