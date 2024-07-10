64ac3d539ff5ee600d88bd2a6d258941d520c5c9
Prathamesh Sonawane <0410pratik@gmail.com>
2024-07-11 00:59:39 +0530

    Implemented accessibility features for improved user experience

--- 07_Word_Scramble/WordScramble/WordScramble/ContentView.swift
+++ 07_Word_Scramble/WordScramble/WordScramble/ContentView.swift
@@ -40,6 +40,9 @@ struct ContentView: View {
                             Image(systemName: "\(word.count).circle")
                             Text(word)
                         }
+                        .accessibilityElement()
+                        .accessibilityLabel(word)
+                        .accessibilityHint("\(word.count) letters")
                     }
                 }
             }
